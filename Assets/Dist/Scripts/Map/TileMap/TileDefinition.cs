0353910577e7276c404328abe5f42a7a38119cbf
Garunnir <65020562+garunnir@users.noreply.github.com>
2026-03-25 18:47:06 +0900

    Add and organize tile SOData assets
    Reorganize and add ScriptableObject tile data into subfolders (Door, Floor, Furniture, Wall) with corresponding .asset and .meta files. Rename TileDefinition.asset into Door/Door.asset, remove the old "New Tile Prefab DB.asset" and add a consolidated "Tile Prefab DB.asset" with updated GUID entries. Update TileDefinition.cs to use Odin Inspector attributes (HorizontalGroup, VerticalGroup, LabelWidth, PreviewField) and reorder fields to surface an inline thumbnail preview and improved inspector layout.

--- Assets/Dist/Scripts/Map/TileMap/TileDefinition.cs
+++ Assets/Dist/Scripts/Map/TileMap/TileDefinition.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace IsoTilemap
 {
     [CreateAssetMenu(fileName = "TileDefinition", menuName = "Iso/Tile Definition")]
     public class TileDefinition : ScriptableObject
     {
-        public string prefabId;
-        public GameObject prefab;
+        [HorizontalGroup("Row", Width = 90)]
+        [PreviewField(90, ObjectFieldAlignment.Left), HideLabel]
         public Sprite thumbnail;
+
+        [HorizontalGroup("Row"), VerticalGroup("Row/Info"), LabelWidth(70)]
+        public string prefabId;
+
+        [VerticalGroup("Row/Info"), LabelWidth(70)]
+        public GameObject prefab;
     }
 }
